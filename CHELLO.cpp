1ee835a414f53a78782f63d86be96db0eba53c47
Hoang Hoang Tuan <hhtuann.19@gmail.com>
2024-03-09 18:44:49 +0700

    add file

---
+++ CHELLO.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long
+
+void solve()
+{
+    // your solution
+    cout << "Hello PTIT.";
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // input
+
+    solve();
+    return 0;
+}
